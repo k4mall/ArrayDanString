791284cf478e24122f82da518a1eca96e2742282
k4mall <kamalbujank@gmail.com>
2025-04-08 13:40:05 +0800

    Update array2A.cpp

--- array2A.cpp
+++ array2A.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
-
+string kelas[30];
+int pos = 0;
+void inputData(string nama);
+void outputData();
 int main(){
-    cout<< "Test";
+    inputData("GT.M.KAMALUDDIN AKBAR");
+    inputData("Indomi goreng plus telor");
+    outputData();
+    inputData("Ayam Bacem Sambal mata");
+    outputData();
+    inputData("Lontong Sayur Lauk Haruan");
+    outputData();
     return 0;
+}
+void inputData(string nama){
+    kelas [pos]=nama;
+    pos++;
+}
+void outputData(){
+    for (int i = 0; i < pos; i++){
+        cout << kelas[i];
+        if (i<pos-1){
+            cout<<",";
+        }else{
+            cout<<"."<<endl;
+        }
+    }
 }
